ac80b626bfd5dc81d1c0f68c1d585d28125e3939
AshakaE <ashakaegerega@gmail.com>
2021-01-03 23:19:13 +0100

    built second section of the main body

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,18 @@ module.exports = {
     extend: {
       colors: {
         primary: "#00A205",
+        lime: "#84CC16",
         secondary: {
+          50: "#949494",
           100: "#656566",
           200: "#333333",
         },
+      },
+      spacing: {
+        84: "22rem",
+        97: "28rem",
+        98: "32rem",
+        100: "36rem",
       },
       fontFamily: {
         body: ["Poppins"],
