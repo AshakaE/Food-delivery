8d3c49d9c7d5ce9210e270e98b904c1267d82d0a
AshakaE <ashakaegerega@gmail.com>
2021-01-03 13:41:55 +0100

    workflow setup

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ["./public/**/*.html"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00A205",
+        secondary: {
+          100: "#656566",
+          200: "#333333",
+        },
+      },
+      fontFamily: {
+        body: ["Poppins"],
+      },
+    },
+  },
+  variants: {
+    extend: {
+      transform: ["active"],
+      translate: ["active"],
+      boxShadow: ["active"],
+    },
+  },
+  plugins: [],
+};
