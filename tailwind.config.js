29f79d976a9e68a58608b4e17e0fdbbf65e94b68
AshakaE <ashakaegerega@gmail.com>
2021-02-03 18:16:35 +0100

    built reservations section

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,10 @@ module.exports = {
     extend: {
       colors: {
         primary: "#00A205",
+        primaryLight: "#AEE1B2",
         lime: "#84CC16",
         secondary: {
+          25: "#fafafa",
           50: "#949494",
           100: "#656566",
           200: "#333333",
@@ -20,6 +22,8 @@ module.exports = {
         97: "28rem",
         98: "32rem",
         100: "36rem",
+        110: "38rem",
+        120: "42rem",
       },
       fontFamily: {
         body: ["Poppins"],
